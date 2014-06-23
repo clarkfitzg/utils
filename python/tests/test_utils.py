0c9b010e01b0ac3cc1255307af055e86e92274ed
Clark Fitzgerald <clarkfitzg@gmail.com>
2014-06-22 21:02:46 -0700

    weighed counter class

--- python/tests/test_utils.py
+++ python/tests/test_utils.py
@@ -1,7 +1,16 @@
 from nose.tools import assert_equal
+from utils import weighcount
 
 
 class test_replicate:
 
     def test_exactly_n(self):
         pass
+
+
+class test_weighcount:
+
+    def setup(self):
+        w = weighcount({'a': 2, 'b': 8, 'c': 10})
+
+
--- python/utils.py
+++ python/utils.py
@@ -7,6 +7,7 @@ Clark Fitzgerald
 '''
 
 import functools
+import collections
 
 
 def replicate(n, func, *args, **kwargs):
@@ -22,3 +23,57 @@ def replicate(n, func, *args, **kwargs):
         itertools.islice
     '''
     return (func(*args, **kwargs) for i in range(n))
+
+
+class weighcount(collections.Counter):
+    '''
+    Returns a subclass of collections.Counter that computes 
+    weights and totals
+
+    >>> a = weighcount({'a': 2, 'b': 8})
+    >>> a.total()
+    10
+    >>> dict(a.weights())
+    {'b': 0.8, 'a': 0.2}
+
+    '''
+    
+    def total(self):
+        '''
+        The sum of all counts
+        '''
+        return sum(self.values())
+
+    def weight(self, key):
+        '''
+        Returns the weight associated with `key`.
+        '''
+        pass
+
+    def weights(self, dynamic=False):
+        '''
+        Generator over (element, weight) tuples
+        '''
+        for key in self:
+            yield key, self[key] / self.total()
+
+    def common_weights(self, n=None):
+        '''
+        List the n most common elements and their weights from the most
+        common to the least.  If n is None, then list all element counts.
+
+        >>> weighcount('abcdeabcdabcaba').common_weights(3)
+
+        Analagous to `most_common` method for counts.
+        '''
+        pass
+
+
+    
+
+
+
+
+
+
+
